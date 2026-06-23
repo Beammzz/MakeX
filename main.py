a7c895c6c7c20732c0aac606459633a57f4ab7ae
Harumi <Sorrathon@proton.me>
2026-06-23 21:26:10 +0700

    fix: correct encoder motor initialization and update comments for clarity

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ from mbuild import power_expand_board
 
 class Kudchan:
     def __init__(self):
-        self.encoder = encoder_motor_class("M6", "INDEX1")
+        self.encoder_upleft = encoder_motor_class("M1", "INDEX1")
+        self.encoder_upright = encoder_motor_class("M2", "INDEX1")
+        self.encoder_downleft = encoder_motor_class("M3", "INDEX1")
+        self.encoder_downright = encoder_motor_class("M4", "INDEX1")
 
     def auto(self, side):
         if side == "L":
@@ -28,10 +31,9 @@ class Kudchan:
 robot = Kudchan()
 # --- Main loop ---
 while True:
-
-    # Check competition mode
+    # Check Automode
     if power_manage_module.is_auto_mode():
-        # ===== AUTONOMOUS MODE =====
+        # ===== AUTO MODE =====
         robot.auto(side="L")  # or side="R"
         pass
 
