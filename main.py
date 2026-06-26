571af53de083480fb016ffc95a55a762299152db
Harumi <Sorrathon@proton.me>
2026-06-26 15:03:26 +0700

    refactor: restructure wheel motor initialization and enhance manual control logic

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ from mbuild import power_expand_board
 
 class Kudchan:
     def __init__(self):
-        self.servo = smartservo_class("M1", "INDEX1")
-        self.encoder = encoder_motor_class("M1", "INDEX1")
+        self.wheel_left_up = encoder_motor_class("M1", "INDEX1")
+        self.wheel_left_bottom = encoder_motor_class("M2", "INDEX1")
+        self.wheel_right_up = encoder_motor_class("M3", "INDEX1")
+        self.wheel_right_bottom = encoder_motor_class("M4", "INDEX1")
 
+        self.wheel_power = 50
+
+    # Useful Functions
+    def set_wheel_power(self, left_up, left_bottom, right_up, right_bottom):
+        self.wheel_left_up.set_power(left_up)
+        self.wheel_left_bottom.set_power(left_bottom)
+        self.wheel_right_up.set_power(right_up)
+        self.wheel_right_bottom.set_power(right_bottom)
+        pass
+
+
+    # Auto Mode
     def auto(self, side):
         if side == "L":
             pass
@@ -21,7 +35,19 @@ class Kudchan:
             pass
         pass
 
+    # Main Control with Controller
     def manual(self):
+        wheel_power = self.wheel_power
+        if gamepad.get_joystick("Ly") > 50:
+            self.set_wheel_power(wheel_power, wheel_power, -wheel_power, -wheel_power)
+        elif gamepad.get_joystick("Ly") < -50:
+            self.set_wheel_power(-wheel_power, -wheel_power, wheel_power, wheel_power)
+        elif gamepad.get_joystick("Lx") > 50:
+            self.set_wheel_power(wheel_power, -wheel_power, wheel_power, -wheel_power)
+        elif gamepad.get_joystick("Lx") < -50:
+            self.set_wheel_power(-wheel_power, wheel_power, -wheel_power, wheel_power)
+        else:
+            self.set_wheel_power(0, 0, 0, 0)
         pass
 
 
@@ -42,7 +68,7 @@ robot = Kudchan()
 
 while True:
     # Main Configurations
-    auto_side: str = "L"  # or "R"
+    auto_side = "L"  # or "R"
 
 
     # Check Automode
